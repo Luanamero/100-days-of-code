73ed248e68ed5d26bdec73aebac28cfdfcb6a3b0
Luana de Méro <luanamero4@gmail.com>
2023-02-14 11:39:07 -0300

    Update Day 2.py

--- Day 2.py
+++ Day 2.py
@@ -1,4 +1,4 @@
-#If the bill was $150.00, split between 5 people, with 12% tip. 
+#Bill splitter calculator
 
 #Each person should pay (150.00 / 5) * 1.12 = 33.6
 #Format the result to 2 decimal places = 33.60
