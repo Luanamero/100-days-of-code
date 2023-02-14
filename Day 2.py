372a072b71176bf406caf6d374cda1cb52ec8acf
Luana de Méro <luanamero4@gmail.com>
2023-02-14 11:39:47 -0300

    Update Day 2.py

--- Day 2.py
+++ Day 2.py
@@ -1,11 +1,5 @@
-#Bill splitter calculator
+#Tip calculator
 
-#Each person should pay (150.00 / 5) * 1.12 = 33.6
-#Format the result to 2 decimal places = 33.60
-
-#Tip: There are 2 ways to round a number. You might have to do some Googling to solve this.💪
-
-#Write your code below this line 👇
 print("Welcome to the tip calculator!")
 
 first_price = float(input("What was the total bill? $"))
