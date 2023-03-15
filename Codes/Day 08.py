1b9d4e5a26f0ecc00ebafec17a911e54f7d3f116
Luana de Méro <luanamero4@gmail.com>
2023-03-15 09:46:38 -0300

    Update Day 08.py

--- Codes/Day 08.py
+++ Codes/Day 08.py
@@ -1,3 +1,4 @@
+#Caesar Cipher Project
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 def ceaser(plain_text, shift_amount, chosen_direction):
